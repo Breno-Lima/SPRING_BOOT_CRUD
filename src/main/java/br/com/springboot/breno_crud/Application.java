0545c1b1c820dce92fdd5eddee78588992f41424
Breno-Lima <brenohslima@hotmail.com>
2022-12-08 04:23:29 -0300

    first and last commit

---
+++ src/main/java/br/com/springboot/breno_crud/Application.java
@@ -0,0 +1,18 @@
+package br.com.springboot.breno_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+    	
+        SpringApplication.run(Application.class, args);/*Linha principal que roda o projerto java spring boot*/
+    }
+   
+    
+}
